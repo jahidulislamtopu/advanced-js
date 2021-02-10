b7cf114672ac91a203ec109e65657f4446644680
Md Jahidul Islam Topu <jahudul15-2753@diu.edu.bd>
2021-02-11 00:29:20 +0600

    splice add

---
+++ array-split.js
@@ -0,0 +1,9 @@
+const nums = [1,2,34,5,6,7,8];
+//cont part = nums.slice(2, 5);
+
+const removed = nums.splice(2,3, 77);
+//console.log(removd);
+//console.log(nums);
+
+const together = nums.join("ami")
+console.log(together);
