269de9926ec436208ebe63216ec6050216d42681
Md Jahidul Islam Topu <jahudul15-2753@diu.edu.bd>
2021-02-11 00:06:10 +0600

    scop

---
+++ scop.js
@@ -0,0 +1,15 @@
+function sum(first, second){
+    let result = first + second + bonus;
+    //sonsole.log(bonus);
+
+    if(result > 9){
+        var mood = "happy";
+        mood = "fishy";
+        mood = "cranky"
+        //console.log(mood);
+    }
+    console.log(day);
+    let day = "friday";
+
+    return result;
+}
